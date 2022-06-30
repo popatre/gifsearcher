2740665f06f556d2f682afe73ed7fa2a814ffc92
popatre <76402245+popatre@users.noreply.github.com>
2022-06-30 19:12:42 +0100

    add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 export default function Footer() {
+    const scrollTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
-        <div className="h-20 b-t-4 border-t-2 border-slate-800 bg-yellow-50 relative mt-20">
-            Footer
+        <div className="h-20 b-t-4 border-t-2 border-slate-800  relative mt-20 bg-slate-100 flex justify-center items-center">
+            <div className="flex w-full justify-evenly">
+                <p onClick={scrollTop} className="underline cursor-pointer">
+                    Back to the top
+                </p>
+                <div className="giphy">
+                    <p className="text-sm">Powered by </p>
+                    <img
+                        src="/images/GIPHY Logo 209px.png"
+                        alt="image"
+                        className="w-16"
+                    />
+                </div>
+            </div>
         </div>
     );
 }
